79c47782d010f5a9b8690fdb44db584c7f21ab08
Flaviu Tamas <tamasflaviu@gmail.com>
2019-03-06 17:37:51 -0500

    Create database schema

--- backend/src/main/java/pw/wp6/avocado_toast/invoker/DatabaseConnection.java
+++ backend/src/main/java/pw/wp6/avocado_toast/invoker/DatabaseConnection.java
@@ -10,7 +10,23 @@ public class DatabaseConnection {
     static {
         try {
             c = DriverManager.getConnection("jdbc:sqlite:file:./db.db");
-            c.prepareCall("CREATE TABLE IF NOT EXISTS").execute();
+            c.prepareCall("CREATE TABLE IF NOT EXISTS users\n" +
+                    "(\n" +
+                    "  id          INTEGER PRIMARY KEY,\n" +
+                    "  name        TEXT,\n" +
+                    "  username    TEXT,\n" +
+                    "  password    TEXT,\n" +
+                    "  ssn         TEXT,\n" +
+                    "  account_type TEXT\n" +
+                    ");\n" +
+                    "CREATE TABLE IF NOT EXISTS ledger_entries\n" +
+                    "(\n" +
+                    "  id       INTEGER PRIMARY KEY,\n" +
+                    "  user_id FOREIGN KEY REFERENCES users,\n" +
+                    "  merchant TEXT,\n" +
+                    "  amount   DECIMAL,\n" +
+                    "  date_time DATETIME\n" +
+                    ");").execute();
         } catch (SQLException e) {
             e.printStackTrace();
             c = null;
